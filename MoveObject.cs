58803914e69a207b33cf24e8071ab134682e75b7
AllberMC <32437364+AllberMC@users.noreply.github.com>
2017-10-23 09:56:41 +0100

    Mi primer script(moviendo el cubo)

---
+++ MoveObject.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections; //Siempre debe estar importada
+using System.Collections.Generic;
+using UnityEngine; //Siempre debe estar importada
+
+public class MoveObject : MonoBehaviour { //Nuestra clase publica esta heredando (:) de la clase MonoBehaviour
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(new Vector3(0f, 30f, 0f)*Time.deltaTime);
+	}
+}
