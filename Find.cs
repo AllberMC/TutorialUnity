d0bcd6d7f037fbaceb4e19ebdf7540f75e5cab85
AllberMC <32437364+AllberMC@users.noreply.github.com>
2017-10-26 11:23:57 +0100

    Búsqueda de varios objetos con la misma etiqueta

--- Find.cs
+++ Find.cs
@@ -7,7 +7,15 @@ public class Find : MonoBehaviour {
     void Awake() {
         // GameObject camera = GameObject.Find("Main Camera"); //Con esto buscamos el objeto por el nombre
         //GameObject camera = GameObject.FindGameObjectWithTag("MainCamera"); //De esta forma buscamos uno o varios objectos mediante etiquetas
-        GameObject camera = Camera.main.gameObject;
-        Debug.Log(camera.transform.position);
+        //GameObject camera = Camera.main.gameObject;
+        //Debug.Log(camera.transform.position);
+
+        //vector de objetos
+        GameObject[] objetos = GameObject.FindGameObjectsWithTag("objetos");
+
+        foreach(GameObject go in objetos)
+        {
+            Debug.Log(go.transform.position);
+        }
     }
 }
